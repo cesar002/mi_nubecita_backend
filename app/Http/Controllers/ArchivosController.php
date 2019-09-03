99a3ef1994a32f0e40587da727625b0bc8f6ce0e
cesar_002 <soul.unleashed13@gmail.com>
2019-09-03 00:37:14 -0500

    Changes to be committed: 	modified:   app/Http/Controllers/ArchivosController.php 	modified:   app/Http/Controllers/AuthController.php 	modified:   database/migrations/2019_08_24_182451_create_archivos_subidos_table.php 	new file:   storage/logs/laravel-2019-09-03.log

--- app/Http/Controllers/ArchivosController.php
+++ app/Http/Controllers/ArchivosController.php
@@ -24,6 +24,7 @@ class ArchivosController extends Controller{
         $fileUpload = null;
         try{
             $filesRepetidos = [];
+            $archivosResp = [];
             $files = $request->allFiles()['files'];
             $cloud = auth()->user()->nubeUsuario()->first();
             $carpetaUsuario = CarpetasUsuarios::where('id_nube', $cloud->id_nube)->where('nombre_carpeta', 'root')->first();
@@ -32,12 +33,20 @@ class ArchivosController extends Controller{
             foreach($files as $file){
                 if(!$this->existFileUpload($carpetaUsuario->id_carpeta, $file->getClientOriginalName(), $file->getMimeType())){
                     $fileUpload = Storage::putFile($cloud->hash_name, $file);
-                    ArchivosSubidos::create([
+                    $archivoSubido = ArchivosSubidos::create([
                         'id_carpeta' => $carpetaUsuario->id_carpeta,
                         'nombre_privado' => basename($fileUpload),
                         'nombre_archivo' => $file->getClientOriginalName(),
                         'tipo_archivo' => $file->getMimeType(),
                         'size_file' => $file->getSize(),
+                    ]);
+                    array_push($archivosResp, [
+                        'idArchivo' =>Crypt::encrypt($archivoSubido->id_archivo),
+                        'nombreCorto' => $this->getNombreCorto($file->getClientOriginalName()),
+                        'nombre' => $file->getClientOriginalName(),
+                        'size' => $file->getSize(),
+                        'fechaSubida' => ArchivosSubidos::where('id_archivo', $archivoSubido->id_archivo)->first()->fecha_subida,
+                        'tipo' =>  substr($file->getMimeType(), strpos($file->getMimeType(), "/")+1),
                     ]);
                 }else{
                     array_push($filesRepetidos, ['fileName' => $file->getClientOriginalName()]);
@@ -46,8 +55,10 @@ class ArchivosController extends Controller{
             DB::commit();
             return response()->json([
                 'status' => 1,
-                'mensaje' => 'Archivo subido con éxito',
+                'mensaje' => 'Archivos subido con éxito',
+                'archivos' => $archivosResp,
                 'repetidos' => $filesRepetidos,
+                'enUso' => $this->getEnUso(),
             ], 200);
         }catch(Exception $e){
             Log::error($e->getMessage());
@@ -79,6 +90,7 @@ class ArchivosController extends Controller{
         foreach($archivos as $archivo){
             array_push($archivosResp, [
                 'idArchivo' => Crypt::encrypt($archivo->id_archivo),
+                'nombreCorto' => $this->getNombreCorto($archivo->nombre_archivo),
                 'nombre' => $archivo->nombre_archivo,
                 'size' => $archivo->size_file,
                 'fechaSubida' => $archivo->fecha_subida,
@@ -89,9 +101,19 @@ class ArchivosController extends Controller{
         return response()->json($archivosResp);
     }
 
+    private function getNombreCorto(string $nombre) : string{
+        return strlen($nombre) >= 10? substr($nombre, 0, 10).'...' : $nombre;
+    }
+
     private function existFileUpload(int $idCarpeta, string $nombreArchivo, string $typeFile) : bool{
         $archivo = ArchivosSubidos::where('id_carpeta', $idCarpeta)->where('nombre_archivo', $nombreArchivo)->where('tipo_archivo', $typeFile)->first();
         return !is_null($archivo);
+    }
+
+    private function getEnUso() : int{
+        $carpeta = auth()->user()->nubeUsuario()->where('id_nube', 1)->first()->carpetas()->where('nombre_carpeta', 'root')->first();
+        $total = $carpeta->archivos->sum('size_file');
+        return $total;
     }
 
     /*
